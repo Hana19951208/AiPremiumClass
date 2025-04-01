670febd6e776959ecbf104fcb1e1ce16b617a9ab
wfe <1948978861@qq.com>
2025-04-01 16:14:06 +0800

    清除多余作业

--- 丁锦权/week05_语言模型/fasttext_train_cooking.stackexchange.py
+++
@@ -1,6 +0,0 @@
-import fasttext
-
-model = fasttext.train_supervised('week5/cooking.stackexchange.txt')
-# 文本分类功能
-print(model.predict("Dangerous pathogens capable of growing in acidic environments"))
-print(model.predict("Which plastic wrap is okay for oven use?"))
--- 张智超/week05/hw3.py
+++
@@ -1,9 +0,0 @@
-import fasttext
-
-# 加载模型
-model = fasttext.load_model('./data/model/cooking_model.bin')
-
-# 进行预测
-label, pred = model.predict(['a bad odor'])
-print(label)
-print(pred)
